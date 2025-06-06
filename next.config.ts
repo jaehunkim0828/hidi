e6f10ee1e7553cc1e8591a21a0836bd0e6591d3a
JaeHun Kim <kkaa81@naver.com>
2025-06-06 16:18:27 +0900

    done

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: '/HiDi', // "username.github.io"가 아니면 반드시 설정
-  assetPrefix: '/HiDi',
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
